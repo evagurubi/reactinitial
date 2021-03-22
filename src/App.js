c49a52c0904f8b782614080f15994400b4c0ecb6
evagurubi <evagurubi@gmail.com>
2021-03-22 15:45:45 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import LoadingMask from "./LoadingMask";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+
+  const fetchData = () => {
+    fetch("api/hotels")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+
+      .catch(function () {
+        setError(true);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      Todo...
+      <h1>Hotels</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
